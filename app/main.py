8ae1e5a86755417bc1000d4291ca26e2a345dfd2
Brian Obot <brianobot@gmail.com>
2025-11-18 14:51:12 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -5,10 +5,14 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment the code below to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    with socket.create_server(("localhost", 6379), reuse_port=True) as server_socket:
+        conn, addr = server_socket.accept()
+        with conn:
+            while True:
+                print(f"Connected to {addr}")
+                _ = conn.recv(1024).decode()
+                conn.sendall(b"+PONG\r\n")
+                
 
 
 if __name__ == "__main__":
